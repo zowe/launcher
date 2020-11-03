627bea196175e33f089bfa0a6c875a425c338d58
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-11-02 23:40:43 -0500

    fix escape
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- .pax/prepare-workspace.sh
+++ .pax/prepare-workspace.sh
@@ -35,12 +35,15 @@ cp -R * "${PAX_WORKSPACE_DIR}/ascii"
 # BRANCH_NAME and BUILD_NUMBER is Jenkins environment variable
 commit_hash=$(git rev-parse --verify HEAD)
 current_timestamp=$(date +%s%3N)
-sed -e "s/\\{\\{build\.branch\\}\\}/${BRANCH_NAME}/g" \
-    -e "s/\\{\\{build\.number\\}\\}/${BUILD_NUMBER}/g" \
-    -e "s/\\{\\{build\.commitHash\\}\\}/${commit_hash}/g" \
-    -e "s/\\{\\{build\.timestamp\\}\\}/${current_timestamp}/g" \
+sed -e "s|{{build\.branch}}|${BRANCH_NAME}|g" \
+    -e "s|{{build\.number}}|${BUILD_NUMBER}|g" \
+    -e "s|{{build\.commitHash}}|${commit_hash}|g" \
+    -e "s|{{build\.timestamp}}|${current_timestamp}|g" \
     "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml" > "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml.tmp"
 mv "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml.tmp" "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml"
+echo "[${SCRIPT_NAME}] manifest:"
+cat "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml"
+echo
 
 # move files shouldn't change encoding to IBM-1047 to content folder
 rsync -rv \

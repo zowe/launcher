7155516a20f568bfb31a9c2357036726cbeb1546
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-12-16 22:47:42 -0500

    fix for compiler on marist
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- .pax/pre-packaging.sh
+++ .pax/pre-packaging.sh
@@ -16,12 +16,16 @@ set -xe
 SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR=$(pwd)
 
+# we use njsc++ shipped with node.js to build
+# we hardcoded where njsc is located, so this pipeline may not work well on other platform
+export PATH=$PATH:/ZOWE/node/node-v14.15.1-os390-s390x/bin
+
 # build
 echo "$SCRIPT_NAME build zowe launcher ..."
 cd "$SCRIPT_DIR/content"
-STEPLIB=CBC.SCCNCMP make
+make -f zosMakefile
 
 # clean up content folder
 echo "$SCRIPT_NAME cleaning up pax folder ..."
 cd "$SCRIPT_DIR/content"
-rm -fr Jenkinsfile Makefile src .git*
+rm -fr Jenkinsfile Makefile zosMakefile src .git*

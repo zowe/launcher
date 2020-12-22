8c11f8f5b81220819c6c9e3eec6327554ed9b4a4
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-12-22 15:46:02 -0500

    try to fix FSUM3008 error
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- .pax/pre-packaging.sh
+++ .pax/pre-packaging.sh
@@ -16,6 +16,12 @@ set -xe
 SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR=$(pwd)
 
+# To solve this error following KC: https://www.ibm.com/support/pages/compiler-invocation-results-error-fsum3067-archive-library-not-found-or-error-fsum3008-incorrect-suffix-used
+# FSUM3008 Specify a file with the correct suffix (.c, .i, .s, .o, .x, .p, .I, or .a), or a corresponding data set name, instead of -L/usr/lpp/cbclib/lib.FSUM3008 Specify a file with the correct suffix (.c, .i, .s, .o, .x, .p, .I, or .a), or a corresponding data set name, instead of -L/usr/lpp/cbclib/lib.
+export _CC_CCMODE=1
+export _CXX_CCMODE=1
+export _C89_CCMODE=1
+
 # build
 echo "$SCRIPT_NAME build zowe launcher ..."
 cd "$SCRIPT_DIR/content"

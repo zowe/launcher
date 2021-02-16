01fd98e7ea741b8afe42f54ffdf508ab7b87493d
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-02-16 17:07:04 +0500

    Use fixed length strings
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- src/manifest.h
+++ src/manifest.h
@@ -1,24 +1,28 @@
 #ifndef MAINFEST_H
 #define MAINFEST_H
 
+#ifndef PATH_MAX
+#define PATH_MAX _POSIX_PATH_MAX
+#endif
+
 typedef struct zl_manifest_commands_t {
-  char *start;
-  char *validate;
+  char start[PATH_MAX+1];
+  char validate[PATH_MAX+1];
 } zl_manifest_commands_t;
 
 typedef struct zl_manifest_build_t {
-  char *branch;
-  char *number;
-  char *commit_hash;
-  char *timestamp;
+  char branch[15+1];
+  char number[15+1];
+  char commit_hash[40+1];
+  char timestamp[13+1];
 } zl_manifest_build_t;
 
 typedef struct zl_manifest_t {
-  char *name;
-  char *id;
-  char *title;
-  char *description;
-  char *license;
+  char name[24+1];
+  char id[16+1];
+  char title[40+1];
+  char description[40+1];
+  char license[16+1];
   zl_manifest_commands_t commands;
   zl_manifest_build_t build;
 } zl_manifest_t;

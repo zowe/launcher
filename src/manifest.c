8b9d83add908cc555ca2afb17d5725068ae16a78
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-02-18 16:58:46 +0500

    Refactor manifest reading
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- src/manifest.c
+++ src/manifest.c
@@ -26,30 +26,26 @@
 #define KEY_LEN 255
 
 static int yaml_read_handler(void *data, unsigned char *buffer, size_t size, size_t *size_read) {
-  FILE * fp = data;
-  int    rc = 1;
+  FILE *fp = data;
+  int rc = 1;
   size_t bytes_read = fread(buffer, 1, size, fp);
   if (bytes_read > 0) {
 #ifdef __MVS__
     if (__etoa_l((char *)buffer, bytes_read) == -1) {
-      printf("error etoa %s\n", strerror(errno));
+      ERROR("error converting manifest -%s\n", strerror(errno));
       rc = 0;
     }
 #endif
   }
   if (ferror(fp)) {
+    ERROR("error reading manifest %s\n", strerror(errno));
     rc = 0;
   }
   *size_read = bytes_read;
   return rc;
 }
 
-char *yaml_pair_get_key(yaml_document_t *document, yaml_node_pair_t *pair) {
-  yaml_node_t *key_node = yaml_document_get_node(document, pair->key);
-  return (char *)key_node->data.scalar.value;
-}
-
-void yaml_pair_copy_key(yaml_document_t *document, yaml_node_pair_t *pair, char *buf, size_t buf_size) {
+static void yaml_pair_copy_key(yaml_document_t *document, yaml_node_pair_t *pair, char *buf, size_t buf_size) {
   yaml_node_t *key_node = yaml_document_get_node(document, pair->key);
   snprintf(buf, buf_size, "%s", (const char *)key_node->data.scalar.value);
 #ifdef __MVS__
@@ -57,17 +53,17 @@ void yaml_pair_copy_key(yaml_document_t *document, yaml_node_pair_t *pair, char 
 #endif
 }
 
-inline bool yaml_is_mapping_node(yaml_node_t *node) {
+inline static bool yaml_is_mapping_node(yaml_node_t *node) {
   return node && node->type == YAML_MAPPING_NODE;
 }
 
-inline bool yaml_is_scalar_node(yaml_node_t *node) {
+inline static bool yaml_is_scalar_node(yaml_node_t *node) {
   return node && node->type == YAML_SCALAR_NODE;
 }
 
-void copy_string_property(yaml_document_t *doc, yaml_node_pair_t *pair, char *buf, size_t buf_size) {
+static void copy_string_property(yaml_document_t *doc, yaml_node_pair_t *pair, char *buf, size_t buf_size) {
   yaml_node_t *node = yaml_document_get_node(doc, pair->value);
-  char *       value = NULL;
+  char *value = NULL;
   if (yaml_is_scalar_node(node)) {
     value = (char *)node->data.scalar.value;
   }
@@ -79,15 +75,15 @@ void copy_string_property(yaml_document_t *doc, yaml_node_pair_t *pair, char *bu
   }
 }
 
-inline yaml_node_pair_t *start_pair(yaml_node_t *node) {
+static inline yaml_node_pair_t *start_pair(yaml_node_t *node) {
   return node->data.mapping.pairs.start;
 }
 
-inline yaml_node_pair_t *end_pair(yaml_node_t *node) {
+static inline yaml_node_pair_t *end_pair(yaml_node_t *node) {
   return node->data.mapping.pairs.top;
 }
 
-void copy_manifest_commands(zl_manifest_commands_t *commands, yaml_document_t *doc, yaml_node_t *node) {
+static void copy_manifest_commands(zl_manifest_commands_t *commands, yaml_document_t *doc, yaml_node_t *node) {
   char key[KEY_LEN + 1];
   for (yaml_node_pair_t *pair = start_pair(node); pair != end_pair(node); pair++) {
     yaml_pair_copy_key(doc, pair, key, sizeof(key));
@@ -101,7 +97,7 @@ void copy_manifest_commands(zl_manifest_commands_t *commands, yaml_document_t *d
   }
 }
 
-void copy_manifest_build(zl_manifest_build_t *build, yaml_document_t *doc, yaml_node_t *node) {
+static void copy_manifest_build(zl_manifest_build_t *build, yaml_document_t *doc, yaml_node_t *node) {
   char key[KEY_LEN + 1];
   for (yaml_node_pair_t *pair = start_pair(node); pair != end_pair(node); pair++) {
     yaml_pair_copy_key(doc, pair, key, sizeof(key));
@@ -117,7 +113,7 @@ void copy_manifest_build(zl_manifest_build_t *build, yaml_document_t *doc, yaml_
   }
 }
 
-void fill_manifest(zl_manifest_t *manifest, yaml_document_t *doc, yaml_node_t *node) {
+static void fill_manifest(zl_manifest_t *manifest, yaml_document_t *doc, yaml_node_t *node) {
   char key[KEY_LEN + 1];
   for (yaml_node_pair_t *pair = start_pair(node); pair != end_pair(node); pair++) {
     yaml_pair_copy_key(doc, pair, key, sizeof(key));
@@ -141,36 +137,50 @@ void fill_manifest(zl_manifest_t *manifest, yaml_document_t *doc, yaml_node_t *n
   }
 }
 
-int load_manifest(const char *filename, zl_manifest_t *manifest) {
-  FILE *          fp = NULL;
-  yaml_parser_t   parser;
+int load_manifest(const char *manifest_file, zl_manifest_t *manifest) {
+  FILE *fp = NULL;
+  yaml_parser_t parser;
   yaml_document_t document;
+  int rc;
+  
+  INFO("loading manifest '%s'\n", manifest_file);
 
-  fp = fopen(filename, "r");
+  fp = fopen(manifest_file, "r");
   if (!fp) {
-    printf("unable to open %s: %s\n", filename, strerror(errno));
+    ERROR("failed to open manifest - %s: %s\n", manifest_file, strerror(errno));
     return -1;
   }
-  printf("Loading '%s': \n", filename);
-  yaml_parser_initialize(&parser);
+  if (!yaml_parser_initialize(&parser)) {
+    ERROR("failed to init YAML parser\n");
+    fclose(fp);
+    return -1;
+  };
   yaml_parser_set_input(&parser, yaml_read_handler, fp);
   if (!yaml_parser_load(&parser, &document)) {
-    fprintf(stderr, "Failed to load document in %s\n", filename);
+    ERROR("failed to parse manifest %s\n", manifest_file);
+    yaml_parser_delete(&parser);
+    fclose(fp);
     return -1;
   }
   yaml_node_t *root = yaml_document_get_root_node(&document);
-  if (root) {
-    if (yaml_is_mapping_node(root)) {
-      fill_manifest(manifest, &document, root);
-      printf("name: %s, id: %s, title: %s, description: %s, license: %s\n", manifest->name, manifest->id, manifest->title,
-             manifest->description, manifest->license);
-      printf("start: %s, validate: %s\n", manifest->commands.start, manifest->commands.validate);
-      printf("branch: %s, number: %s, commitHash: %s, timestamp %s\n", manifest->build.branch, manifest->build.number,
-             manifest->build.commit_hash, manifest->build.timestamp);
-    } else {
-      printf("invalid document structure: %s\n", filename);
+  do {
+    if (!root) {
+      ERROR("failed to get root node in manifest %s\n", manifest_file);
+      rc = -1;
+      break;
     }
-  }
+    if (!yaml_is_mapping_node(root)) {
+      ERROR("failed to find mapping node in manifest %s\n", manifest_file);
+      rc = -1;
+      break;
+    }
+    fill_manifest(manifest, &document, root);
+    INFO("manifest %s loaded - name: %s, id: %s, title: %s, description: %s, license: %s\n", manifest_file,
+         manifest->name, manifest->id, manifest->title, manifest->description, manifest->license);
+    DEBUG("manifest scripts - start: %s, validate: %s\n", manifest->commands.start, manifest->commands.validate);
+    DEBUG("manifest build - branch: %s, number: %s, commitHash: %s, timestamp %s\n", manifest->build.branch,
+          manifest->build.number, manifest->build.commit_hash, manifest->build.timestamp);
+  } while(0);
   yaml_document_delete(&document);
   yaml_parser_delete(&parser);
   fclose(fp);

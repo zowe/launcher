8eecab6210e14525731b576f4a3d67a65f732f56
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-04-15 16:32:40 -0400

    avoid to run install.sh out of zos
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- bin/install.sh
+++ bin/install.sh
@@ -10,51 +10,55 @@
 #  Copyright Contributors to the Zowe Project.
 ################################################################################
 
-if [ -z "${ZOWE_ROOT_DIR}" ]; then
-  >&2 echo "Error: ZOWE_ROOT_DIR is required"
-  exit 1
-fi
-if [ -z "${ZOWE_DSN_PREFIX}" ]; then
-  >&2 echo "Error: ZOWE_DSN_PREFIX is required"
-  exit 1
-fi
+if [ `uname` = "OS/390" ]; then
 
-# we should already in component directory
-if [ ! -f bin/zowe_launcher ]; then
-  >&2 echo "Error: Zowe launcher program is missing"
-  exit 1
-fi
-if [ ! -f samplib/ZWESLSTC ]; then
-  >&2 echo "Error: ZWESLSTC procedure samplib is missing"
-  exit 1
-fi
+  if [ -z "${ZOWE_ROOT_DIR}" ]; then
+    >&2 echo "Error: ZOWE_ROOT_DIR is required"
+    exit 1
+  fi
+  if [ -z "${ZOWE_DSN_PREFIX}" ]; then
+    >&2 echo "Error: ZOWE_DSN_PREFIX is required"
+    exit 1
+  fi
 
-script=launcher/install.sh
+  # we should already in component directory
+  if [ ! -f bin/zowe_launcher ]; then
+    >&2 echo "Error: Zowe launcher program is missing"
+    exit 1
+  fi
+  if [ ! -f samplib/ZWESLSTC ]; then
+    >&2 echo "Error: ZWESLSTC procedure samplib is missing"
+    exit 1
+  fi
 
-# copy samplib to SZWESAMP
-if [ -z "${LOG_FILE}" ]; then
-  cp -v samplib/ZWESLSTC "//'${ZOWE_DSN_PREFIX}.SZWESAMP'"
-  rc=$?
-else
-  cp -v samplib/ZWESLSTC "//'${ZOWE_DSN_PREFIX}.SZWESAMP'" >> ${LOG_FILE}
-  rc=$?
-fi
-if [ "${rc}" = "0" ] ; then
-  print_and_log_message "$script ZWESLSTC copied to ${ZOWE_DSN_PREFIX}.SZWESAMP"
-else
-  print_and_log_message "$script ZWESLSTC not copied to ${ZOWE_DSN_PREFIX}.SZWESAMP, RC=$rc"
-fi
+  script=launcher/install.sh
 
-# copy launcher program to 
-if [ -z "${LOG_FILE}" ]; then
-  cp -X bin/zowe_launcher "//'${ZOWE_DSN_PREFIX}.SZWEAUTH(ZWELNCH)'"
-  rc=$?
-else
-  cp -X bin/zowe_launcher "//'${ZOWE_DSN_PREFIX}.SZWEAUTH(ZWELNCH)'" >> ${LOG_FILE}
-  rc=$?
-fi
-if [ "${rc}" = "0" ] ; then
-  print_and_log_message "$script ZWELNCH program copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH"
-else
-  print_and_log_message "$script ZWELNCH program not copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH, RC=$rc"
+  # copy samplib to SZWESAMP
+  if [ -z "${LOG_FILE}" ]; then
+    cp -v samplib/ZWESLSTC "//'${ZOWE_DSN_PREFIX}.SZWESAMP'"
+    rc=$?
+  else
+    cp -v samplib/ZWESLSTC "//'${ZOWE_DSN_PREFIX}.SZWESAMP'" >> ${LOG_FILE}
+    rc=$?
+  fi
+  if [ "${rc}" = "0" ] ; then
+    print_and_log_message "$script ZWESLSTC copied to ${ZOWE_DSN_PREFIX}.SZWESAMP"
+  else
+    print_and_log_message "$script ZWESLSTC not copied to ${ZOWE_DSN_PREFIX}.SZWESAMP, RC=$rc"
+  fi
+
+  # copy launcher program to 
+  if [ -z "${LOG_FILE}" ]; then
+    cp -X bin/zowe_launcher "//'${ZOWE_DSN_PREFIX}.SZWEAUTH(ZWELNCH)'"
+    rc=$?
+  else
+    cp -X bin/zowe_launcher "//'${ZOWE_DSN_PREFIX}.SZWEAUTH(ZWELNCH)'" >> ${LOG_FILE}
+    rc=$?
+  fi
+  if [ "${rc}" = "0" ] ; then
+    print_and_log_message "$script ZWELNCH program copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH"
+  else
+    print_and_log_message "$script ZWELNCH program not copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH, RC=$rc"
+  fi
+
 fi

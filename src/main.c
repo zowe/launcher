461ab2f7e5a87f0a1a11bd1f523150bdd42ecb13
Leanid Astrakou <lastrakou@rocketsoftware.com>
2023-08-01 10:19:44 -0400

    Added better wtoPrintF code
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- src/main.c
+++ src/main.c
@@ -370,6 +370,47 @@ void message(char *message){
   );
 }
 
+#define WTO_MAX_SIZE 126
+void wtoPrintf(char *formatString, ...){
+  char text[WTO_MAX_SIZE+1];       /* Allow for trailing null character */
+  va_list argPointer;
+  int cnt;
+
+  for (int pass=0; pass<2; pass++){
+
+    /* The resulting text string from vsnprintf is unpredictable if
+       there is an error in the format string or arguments.  In that
+       case we will set the output text area to null, repeat the
+       vsnprintf, and then find the length of the null terminated
+       string.  This avoids initializing the output text area prior
+       to every successful request.
+    */
+
+    va_start(argPointer,formatString);
+    cnt = vsnprintf(text,sizeof(text),formatString,argPointer);
+    va_end(argPointer);
+
+    if (cnt<0){
+      if (pass==0)
+        memset(text,0,sizeof(text));  /* Clear the text buffer before retrying the vsnprint request */
+      else {
+        text[WTO_MAX_SIZE] = 0;       /* Ensure strlen stops at the end of the text buffer */
+        cnt = strlen(text);           /* Find the end of the text string */
+      }
+    } else
+      break;                          /* vsnprintf did not return an error - cnt was set */
+  }
+  if (cnt>WTO_MAX_SIZE)               /* If more data to format than the text buffer length */
+    cnt = WTO_MAX_SIZE;               /* Truncate the formatted length to the text buffer length */
+
+  /* We never want to include a final \n character in the WTO text */
+
+  if (cnt>0 && text[cnt-1] == '\n')   /* If text ends with \n */
+    text[cnt-1] = 0;                  /* Change it into a null character */
+
+  message(text);
+}
+
 static void set_shared_uss_env(ConfigManager *configmgr) {
   Json *env = NULL;
   int cfgGetStatus = cfgGetAnyC(configmgr, ZOWE_CONFIG_NAME, &env, 2, "zowe", "environments");
@@ -884,7 +925,7 @@ static int start_component(zl_comp_t *comp) {
 
   INFO(MSG_COMP_STARTED, comp->name);
   INFO("THIS IS A TEST AND SHOULD BE REMOVED!!!!!!!!!!!!!!!!!");
-  message("SYSLOG POSSIBLY????????????????????");
+  wtoPrintf("SYSLOG POSSIBLY????????????????????");
 
   if (pthread_create(&comp->comm_thid, NULL, handle_comp_comm, comp) != 0) {
     DEBUG("comm thread not started for %s - %s\n", comp->name, strerror(errno));

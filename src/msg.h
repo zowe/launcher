23cd2e422fab310328f61272eb0d8c057047ce37
1000TurquoisePogs <sgrady@rocketsoftware.com>
2025-04-15 14:00:43 -0400

    Oops, shell comment in c file
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- src/msg.h
+++ src/msg.h
@@ -79,7 +79,7 @@
 #define MSG_CMD_RUN_ERR         MSG_PREFIX "0064E" " failed to run command %s - %s\n"
 #define MSG_CMD_OUT_ERR         MSG_PREFIX "0065E" " error reading output from command '%s' - %s\n"
 #define MSG_CMD_RCP_WARN        MSG_PREFIX "0066W" " command '%s' ended with code %d\n"
-# 67E retired, parmlib member name restriction removed
+// 67E retired, parmlib member name restriction removed
 #define MSG_MEMBER_MISSING      MSG_PREFIX "0068E" " PARMLIB() entries must have a member name\n"
 #define MSG_CFG_VALID           MSG_PREFIX "0069I" " Configuration is valid\n"
 #define MSG_CFG_INVALID         MSG_PREFIX "0070E" " Configuration has validity exceptions:\n"

6106ae1791e477f00f125b94c1026721a9e6e515
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2025-07-31 08:48:38 +0200

    Comment the code
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- src/main.c
+++ src/main.c
@@ -244,12 +244,19 @@ static bool check_match_and_wto_message(const char* sys_message_id, const char* 
   if (zl_context.trim_sys_message) {
     printf_wto(input_string + sys_message_pos);
   } else {
+    // Short message, WTO *
     if (input_string_len <= WTO_MESSAGE_LENGTH) {
       printf_wto(input_string);
+    // Message length > WTO_MESSAGE_LENGTH
     } else {
+      // After the match, there are more chars than WTO_MESSAGE_LENGTH
+      // WTO from match position
       if (input_string_len - sys_message_pos > WTO_MESSAGE_LENGTH) {
         printf_wto(input_string + sys_message_pos);
       } else {
+        // The match is in the last WTO_MESSAGE_LENGTH chars
+        // WTO last WTO_MESSAGE_LENGTH chars - egde case: if the match is last word
+        //   user will see the text before match too
         printf_wto(input_string + (input_string_len - WTO_MESSAGE_LENGTH));
       }
     }

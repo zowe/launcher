8ca35246e54c0d2fb9689af397ab1172248f4665
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2025-09-25 14:58:17 +0200

    More test cases
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- test/environments.sh
+++ test/environments.sh
@@ -29,11 +29,10 @@ if [ ! -z "${print}" ]; then
     "${LAUNCHER}" "ha1"
 fi
 
-LAUNCHER_OUTPUT=$("${LAUNCHER}" "ha1" 2>&1 | grep "DEBUG shared env pos.*TEST_VAR.*")
-
+# awk prints between TEST_VAR_START= and TEST_VAR_END=
+LAUNCHER_OUTPUT=$("${LAUNCHER}" "ha1" 2>&1 | awk '/TEST_VAR_START=/{flag=1}/TEST_VAR_END=/{print; flag=0}flag')
 printf "%s\n\n" "${LAUNCHER_OUTPUT}"
 
-printf "${ZOWE_YAML} TEST_VAR*:\n"
-cat "${ZOWE_YAML}" | grep TEST_VAR
+awk '/TEST_VAR_START/{flag=1}/TEST_VAR_END/{print; flag=0}flag' "${ZOWE_YAML}"
 
 exit 0

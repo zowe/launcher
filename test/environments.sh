fb5ae68d96548db6d266a93bc70c77d10c3db270
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2025-09-29 11:16:04 +0200

    Unix variable names bug
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- test/environments.sh
+++ test/environments.sh
@@ -13,6 +13,7 @@
 # ---------------------------------------------------
 
 print=
+errors=0
 
 if [ ! -z "${1}" ]; then
     print='1'
@@ -27,17 +28,39 @@ fi
 ZOWE_YAML="./files/zowe.environments.yaml"
 
 LAUNCHER_OUTPUT=
+LAUNCHER_OUTPUT_TEST_VAR=
+LAUNCHER_OUTPUT_KEYS=
 export ZLDEBUG='ON'
 export CONFIG="FILE(${ZOWE_YAML})"
 
+LAUNCHER_OUTPUT=$("${LAUNCHER}" "ha1" 2>&1)
+
 if [ ! -z "${print}" ]; then
-    "${LAUNCHER}" "ha1"
+    printf "%s" "${LAUNCHER_OUTPUT}"
 fi
 
 # awk prints between TEST_VAR_START= and TEST_VAR_END=
-LAUNCHER_OUTPUT=$("${LAUNCHER}" "ha1" 2>&1 | awk '/TEST_VAR_START=/{flag=1}/TEST_VAR_END=/{print; flag=0}flag')
-printf "%s\n\n" "${LAUNCHER_OUTPUT}"
+LAUNCHER_OUTPUT_TEST_VAR=$(printf "%s" "${LAUNCHER_OUTPUT}" | awk '/TEST_VAR_START=/{flag=1}/TEST_VAR_END=/{print; flag=0}flag')
+printf "%s\n\n" "${LAUNCHER_OUTPUT_TEST_VAR}"
 
 awk '/TEST_VAR_START/{flag=1}/TEST_VAR_END/{print; flag=0}flag' "${ZOWE_YAML}"
 
-exit 0
+printf "\n---zowe.environments.* expected to be ignored ---\n\n"
+
+LAUNCHER_OUTPUT_KEYS=$(printf "%s" "${LAUNCHER_OUTPUT}" | grep -e 'Key in configuration')
+
+while read -r line; do
+    if [ -n "$(echo "${line}" | grep -e '<ignored>')" ]; then
+        key="$(echo "${line}" | awk -F: '{ print $1}')"
+        key="\`zowe.environments.${key}\`"
+        matchKey="$(printf "%s" "${LAUNCHER_OUTPUT_KEYS}" | grep -e "${key}")"
+        if [ -n "${matchKey}" ]; then
+            printf "Key '%s' found:\n  %s\n" "${key}" "${matchKey}"
+        else
+            echo "Key '${key}' not found!"
+            errors=`expr $errors + 1`
+        fi
+    fi
+done < "${ZOWE_YAML}"
+
+exit $errors

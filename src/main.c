9e0cdd8d184021dc38d35f8d41052aa5e797c1ed
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2026-05-06 08:13:12 +0200

    Use only ZL_INT_ARRAY_CAPACITY items
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- src/main.c
+++ src/main.c
@@ -705,6 +705,10 @@ static void init_component_restart_intervals(zl_comp_t *comp, ConfigManager *con
   // load restartIntervals from the configuration
   JsonArray *intArray = jsonAsArray(restartIntArray);
   int count = jsonArrayGetCount(intArray);
+  if (count > ZL_INT_ARRAY_CAPACITY) {
+    count = ZL_INT_ARRAY_CAPACITY;
+    DEBUG("zowe.launcher.restartIntervals: %d out of %d will be used.", ZL_INT_ARRAY_CAPACITY, count);
+  }
   comp->restart_intervals.count = count;
   for (int i = 0; i < count; i++) {
     comp->restart_intervals.data[i] = jsonArrayGetNumber(intArray, i);

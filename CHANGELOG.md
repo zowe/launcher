0b874f2bef64d77edd1a5511968c2eb58880798c
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2026-05-11 14:20:49 +0200

    One pass loop
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,8 @@
 All notable changes to the Zowe Launcher package will be documented in this file.
 This repo is part of the app-server Zowe Component, and the change logs here may appear on Zowe.org in that section.
 
-## 3.5.0
-- Bugfix: Escape backslash when used in environment variable ([#168](https://github.com/zowe/launcher/pull/168))
+## 3.6.0
+- Bugfix: Escape backslash, backtick and dollar sign when used in environment variable ([#168](https://github.com/zowe/launcher/pull/168))
 
 ## 3.4.0
 - Enhancement: Message `ZWEL0021I` includes the version of launcher ([#167](https://github.com/zowe/launcher/pull/167))
--- src/main.c
+++ src/main.c
@@ -453,27 +453,18 @@ static bool arrayListContains(ArrayList *list, char *element) {
 
 static char* escape_string(char *input) {
     int length = strlen(input);
-    int quotes = 0;
-    int backSlashes = 0;
-    for (int i = 0; i < length; i++) {
-        if (input[i] == '\"') quotes++;
-        if (input[i] == '\\') backSlashes++;
-    }
-
-    // 2 + 1 = add quote on first and the last position and \0 at the end
-    // quotes & backSlashes = add '\' to escape '"' and '\'
-    char *output = malloc(length + quotes + backSlashes + 2 + 1);
+    // Worst case: every character needs escaping
+    char *output = malloc(length * 2 + 2 + 1);
     output[0] = '\"';
     int j = 1;
     for (int i = 0; i < length; i++) {
-        if (input[i] == '\"' || input[i] == '\\') {
+        if (input[i] == '\"' || input[i] == '\\' || input[i] == '$' || input[i] == '`') {
             output[j++] = '\\';
         }
         output[j++] = input[i];
     }
     output[j++] = '\"';
     output[j++] = 0;
-
     return output;
 }
 

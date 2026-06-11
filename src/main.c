59e58024fae2e493390eff6bd4f7b18fb644beb5
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2026-06-11 16:46:07 +0200

    Check possible short string
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- src/main.c
+++ src/main.c
@@ -276,7 +276,7 @@ static bool check_match_and_wto_message(const char* sys_message_id, const char* 
 
   if (other_messages) {
     // App-server -> Show Environment -> E.g. ^ZWE_zowe_sysMessages_0=ZWEL0021I$
-    if (memcmp(ZWE_ZOWE_SYS_MESSAGES, input_string, ZWE_ZOWE_SYS_MESSAGES_LEN) == 0) {
+    if (strncmp(input_string, ZWE_ZOWE_SYS_MESSAGES, ZWE_ZOWE_SYS_MESSAGES_LEN) == 0) {
       return false;
     }
   }
